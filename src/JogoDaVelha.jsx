99dbfb8295d1979cdf65dcecc0792915a9e82fda
Mario Torres <scaramuzzini@gmail.com>
2025-04-09 11:05:08 -0400

    logica vencedor linha 1

--- src/JogoDaVelha.jsx
+++ src/JogoDaVelha.jsx
@@ -8,23 +8,48 @@ function Quadrado({q, handleClick}) {
         </button>
 }
 
+function verificarVencedor(quadrados) {
+    // && AND E
+    if (quadrados[0] == quadrados[1] 
+        && quadrados[1] == quadrados[[2]]) {
+            //temos um vencedor
+            return quadrados[0];
+    }
+    return null;      
+}
+
 function Tabuleiro() {
     const [vezDoX, setVezDoX] = useState(true);
     const [quadrados, setQuadrados] = useState(Array(9).fill(null))
     function handleClick(i) {
+        if (verificarVencedor(quadrados)) {
+            return;
+        }
+
         if (quadrados[i] == null) {
             const novosQuadrados = quadrados.slice();
-            if (vezDoX) {
-                novosQuadrados[i] = 'X';
-            } else {
-                novosQuadrados[i] = 'O';
-            }
+            novosQuadrados[i] = vezDoX ? 'X' : 'O'
             setVezDoX(!vezDoX)
             setQuadrados(novosQuadrados);
         }
     }
+    function reiniciarJogo() {
+        setQuadrados(Array(9).fill(null));
+        setVezDoX(true);
+    }
+
+    let vencedor = verificarVencedor(quadrados);
+    let mensagem;
+    if (vencedor) {
+        mensagem = 'Vencedor é '+vencedor;
+    } else {
+        mensagem = 'Agora é a vez de ' + (vezDoX ? 'X' : 'O')
+    }
+
     return (
         <div>
+            <button onClick={reiniciarJogo}>Reiniciar jogo</button>
+            <h2>{mensagem}</h2>
             <div className="linha">
                 <Quadrado q={quadrados[0]} handleClick={() => handleClick(0)} />
                 <Quadrado q={quadrados[1]} handleClick={() => handleClick(1)} />
@@ -49,7 +74,6 @@ export default function JogoDaVelha() {
     return (
         <>
             <h1>Jogo da velha</h1>
-            <p>Este é um jogo...</p>
             <Tabuleiro />
         </>
     )
