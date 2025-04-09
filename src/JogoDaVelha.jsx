4680728e786f21deaa57bbde766a64386e554beb
Mario Torres <scaramuzzini@gmail.com>
2025-04-09 11:46:22 -0400

    Animação no vencedor

--- src/JogoDaVelha.jsx
+++ src/JogoDaVelha.jsx
@@ -1,15 +1,19 @@
 import './jogo.css'
 import { useState } from 'react';
 
-function Quadrado({q, handleClick}) {
-    return <button className='q'
-        onClick={handleClick}>
+function Quadrado({ q, handleClick, vencedor }) {
+    let classes = 'q';
+    if(vencedor) {
+        classes += ' vencedor';
+    }
+    return (
+        <button className={classes} onClick={handleClick}>
             {q}
         </button>
+    );
 }
 
 function verificarVencedor(quadrados) {
-
     const combinacoesVencedoras = [
         // linhas
         [0, 1, 2], 
@@ -34,7 +38,7 @@ function verificarVencedor(quadrados) {
             && quadrados[p2] == quadrados[p3]) {
                 console.log('temos um vencedor!!! ' + quadrados[p1])
                 //temos um vencedor
-                return quadrados[p1];
+                return { jogador: quadrados[p1], linhaVencedora: combinacoesVencedoras[i] };
         }
     }
 
@@ -43,28 +47,34 @@ function verificarVencedor(quadrados) {
 
 function Tabuleiro() {
     const [vezDoX, setVezDoX] = useState(true);
-    const [quadrados, setQuadrados] = useState(Array(9).fill(null))
+    const [quadrados, setQuadrados] = useState(Array(9).fill(null));
+    const [linhaVencedora, setLinhaVencedora] = useState(null);
+
     function handleClick(i) {
-        if (verificarVencedor(quadrados)) {
+        if (verificarVencedor(quadrados) || quadrados[i] != null) {
             return;
         }
 
-        if (quadrados[i] == null) {
-            const novosQuadrados = quadrados.slice();
-            novosQuadrados[i] = vezDoX ? 'X' : 'O'
-            setVezDoX(!vezDoX)
-            setQuadrados(novosQuadrados);
-        }
+        const novosQuadrados = quadrados.slice();
+        novosQuadrados[i] = vezDoX ? 'X' : 'O'
+        setVezDoX(!vezDoX)
+        setQuadrados(novosQuadrados);
+        
+        const resultadoVencedor = verificarVencedor(novosQuadrados);
+         if(resultadoVencedor){
+            setLinhaVencedora(resultadoVencedor.linhaVencedora);
+         }
     }
     function reiniciarJogo() {
         setQuadrados(Array(9).fill(null));
         setVezDoX(true);
+        setLinhaVencedora(null);
     }
 
     let vencedor = verificarVencedor(quadrados);
     let mensagem;
     if (vencedor) {
-        mensagem = 'Vencedor é '+vencedor;
+        mensagem = 'Vencedor é '+vencedor.jogador;
     } else {
         mensagem = 'Agora é a vez de ' + (vezDoX ? 'X' : 'O')
     }
@@ -74,19 +84,19 @@ function Tabuleiro() {
             <button onClick={reiniciarJogo}>Reiniciar jogo</button>
             <h2>{mensagem}</h2>
             <div className="linha">
-                <Quadrado q={quadrados[0]} handleClick={() => handleClick(0)} />
-                <Quadrado q={quadrados[1]} handleClick={() => handleClick(1)} />
-                <Quadrado q={quadrados[2]} handleClick={() => handleClick(2)} />
+                <Quadrado q={quadrados[0]} handleClick={() => handleClick(0)} vencedor={linhaVencedora && linhaVencedora.includes(0)} />
+                <Quadrado q={quadrados[1]} handleClick={() => handleClick(1)} vencedor={linhaVencedora && linhaVencedora.includes(1)} />
+                <Quadrado q={quadrados[2]} handleClick={() => handleClick(2)} vencedor={linhaVencedora && linhaVencedora.includes(2)} />
             </div>
             <div className="linha">
-                <Quadrado q={quadrados[3]} handleClick={() => handleClick(3)} />
-                <Quadrado q={quadrados[4]} handleClick={() => handleClick(4)} />
-                <Quadrado q={quadrados[5]} handleClick={() => handleClick(5)} />
+                <Quadrado q={quadrados[3]} handleClick={() => handleClick(3)} vencedor={linhaVencedora && linhaVencedora.includes(3)} />
+                <Quadrado q={quadrados[4]} handleClick={() => handleClick(4)} vencedor={linhaVencedora && linhaVencedora.includes(4)} />
+                <Quadrado q={quadrados[5]} handleClick={() => handleClick(5)} vencedor={linhaVencedora && linhaVencedora.includes(5)} />
             </div>
             <div className="linha">
-                <Quadrado q={quadrados[6]} handleClick={() => handleClick(6)} />
-                <Quadrado q={quadrados[7]} handleClick={() => handleClick(7)} />
-                <Quadrado q={quadrados[8]} handleClick={() => handleClick(8)} />
+                <Quadrado q={quadrados[6]} handleClick={() => handleClick(6)} vencedor={linhaVencedora && linhaVencedora.includes(6)} />
+                <Quadrado q={quadrados[7]} handleClick={() => handleClick(7)} vencedor={linhaVencedora && linhaVencedora.includes(7)} />
+                <Quadrado q={quadrados[8]} handleClick={() => handleClick(8)} vencedor={linhaVencedora && linhaVencedora.includes(8)} />
             </div>
         </div>
     )
